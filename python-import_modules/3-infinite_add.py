124e7edcd6b75e587a941fd895199012df4fe72e
ChukwukaJ <117227123+ChukwukaJ@users.noreply.github.com>
2023-03-10 23:34:10 +0200

    Update 3-infinite_add.py

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -3,10 +3,10 @@
 import sys
 
 if __name__ == '__main__':
-args = sys.argv[1:]
+    args = sys.argv[1:]
 
-# Convert each argument to an integer and sum them up
-total = sum(int(arg) for arg in args)
+    # Convert each argument to an integer and sum them up
+    total = sum(int(arg) for arg in args)
 
-# Print the total
-print(total)
+    # Print the total
+    print(total)
