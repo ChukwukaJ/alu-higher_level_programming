a326a5e6b4d033958dd44696e9199ee425e3db91
ChukwukaJ <117227123+ChukwukaJ@users.noreply.github.com>
2023-03-18 11:25:18 +0100

    Update 3-rectangle.py

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -1,31 +1,68 @@
 #!/usr/bin/python3
-# 3-square.py
-"""A module that defines a square """
+"""A class that defines a rectangle"""
 
 
-class Square:
-    """A class that represents a square"""
+class Rectangle:
+    """this represents a rectangle"""
 
-    def __init__(self, size=0):
-        """Initializing this square class
+    def __init__(self, width=0, height=0):
+        """Initializing this rectangle class
         Args:
-            size: represnets the size of the square defined
+            width: represents the width of the rectangle
+            height: represents the height of the rectangle
         Raises:
             TypeError: if size is not integer
             ValueError: if size is less than zero
         """
+        self.width = width
+        self.height = height
 
-        if not isinstance(size, int):
-            raise TypeError('size must be an integer')
-        if size < 0:
-            raise ValueError('size must be >= 0')
+    @property
+    def width(self):
+        """retrieves width attribute"""
+        return self.__width
 
-        self.__size = size
+    @width.setter
+    def width(self, value):
+        """sets width attribute"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """retrieves height attribute"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """sets height attribute"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
 
     def area(self):
-        """
-        Calculate area of the square
-        Returns: The square of the size
-        """
+        """Returns the area of the rectangle"""
+        return (self.__width * self.__height)
 
-        return (self.__size ** 2)
+    def perimeter(self):
+        """Returns the perimeter of the rectangle"""
+        if self.__width == 0 or self.__height == 0:
+            return (0)
+        return ((self.__width * 2) + (self.__height * 2))
+
+    def __str__(self) -> str:
+        """presents a diagram of the rectangle defined for an object"""
+        if self.__width == 0 or self.__height == 0:
+            return ("")
+        rectangle = ""
+        for column in range(self.__height):
+            for row in range(self.__width):
+                rectangle += "#"
+            if column < self.__height - 1:
+                rectangle += "\n"
+        return (rectangle)
