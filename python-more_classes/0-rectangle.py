87483153d8b5544b28a45a82f7032ea9c64f58fb
ChukwukaJ <117227123+ChukwukaJ@users.noreply.github.com>
2023-03-18 11:20:28 +0100

    Update 0-rectangle.py

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""Define a class Square."""
+"""Defines a Rectangle class."""
 
 
-class Square:
-    """Represent a square."""
+class Rectangle:
+    """Represent a rectangle."""
     pass
