dcbc37a269ff7fe3c47f0ce74738a3be8b8547ce
ChukwukaJ <117227123+ChukwukaJ@users.noreply.github.com>
2023-03-18 11:26:48 +0100

    Update 5-rectangle.py

--- python-more_classes/5-rectangle.py
+++ python-more_classes/5-rectangle.py
@@ -1,54 +1,75 @@
 #!/usr/bin/python3
-# 5-square.py
-"""A module that defines a square """
+"""Defines a Rectangle class."""
 
 
-class Square:
-    """A class that represents a square"""
+class Rectangle:
+    """Represent a rectangle."""
 
-    def __init__(self, size=0):
-        """Initializing this square class
+    def __init__(self, width=0, height=0):
+        """Initialize a new Rectangle.
         Args:
-            size: represnets the size of the square defined
-        Raises:
-            TypeError: if size is not integer
-            ValueError: if size is less than zero
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
         """
-
-        if not isinstance(size, int):
-            raise TypeError('size must be an integer')
-        if size < 0:
-            raise ValueError('size must be >= 0')
-
-        self.__size = size
+        self.width = width
+        self.height = height
 
     @property
-    def size(self):
-        """Retrieves size of square"""
+    def width(self):
+        """Get/set the width of the Rectangle."""
+        return self.__width
 
-        return self.__size
-
-    @size.setter
-    def size(self, value):
+    @width.setter
+    def width(self, value):
         if not isinstance(value, int):
-            raise TypeError('size must be an integer')
+            raise TypeError("width must be an integer")
         if value < 0:
-            raise ValueError('size must be >= 0')
-        self.__size = value
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get/set the height of the Rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
 
     def area(self):
+        """Return the area of the Rectangle."""
+        return (self.__width * self.__height)
+
+    def perimeter(self):
+        """Return the perimeter of the Rectangle."""
+        if self.__width == 0 or self.__height == 0:
+            return (0)
+        return ((self.__width * 2) + (self.__height * 2))
+
+    def __str__(self):
+        """Return the printable representation of the Rectangle.
+        Represents the rectangle with the # character.
         """
-        Calculate area of the square
-        Returns: The square of the size
-        """
+        if self.__width == 0 or self.__height == 0:
+            return ("")
 
-        return (self.__size ** 2)
+        rect = []
+        for i in range(self.__height):
+            [rect.append('#') for j in range(self.__width)]
+            if i != self.__height - 1:
+                rect.append("\n")
+        return ("".join(rect))
 
-    def my_print(self):
-        """print the square in # """
+    def __repr__(self):
+        """Return the string representation of the Rectangle."""
+        rect = "Rectangle(" + str(self.__width)
+        rect += ", " + str(self.__height) + ")"
+        return (rect)
 
-        if self.__size == 0:
-            print()
-
-        for i in range(self.__size):
-            print("#" * self.__size)
+    def __del__(self):
+        """Print a message for every deletion of a Rectangle."""
+        print("Bye rectangle...")
