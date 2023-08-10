4a7612d80f871e51093854f65333d8f25dd5dbb0
ChukwukaJ <c.nwachukwu1@alustudent.com>
2023-08-10 08:56:59 -0700

    done

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,36 +1,18 @@
 #!/usr/bin/python3
-"""Defines a square-printing function."""
+"""Defines a function that prints a square with the # character"""
 
 
 def print_square(size):
-    """
-    Prints a square using the "#" character.
-
-    Args:
-        size: An integer that represents the size length of the square.
-
-    Raises:
-        TypeError: If size is not an integer.
-        ValueError: If size is less than 0.
-        TypeError: If size is a float and less than 0.
-
-    Returns:
-        None.
-
-    Example:
-        >>> print_square(4)
-        ####
-        ####
-        ####
-        ####
+    """Prints a square with the # character.
+    :para: size
+    :type size: int
+    :raises: TypeError if size is not an integer
+    :raises: ValueError if size is less than zero
     """
     if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
-
-    if size == 0:
-        return
-
-    for i in range(size):
-        print("#" * size)
+    for k in range(size):
+        [print("#", end="") for k in range(size)]
+        print("")
