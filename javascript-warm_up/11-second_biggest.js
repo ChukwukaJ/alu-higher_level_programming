4dec956675f2ef3dc195d73b78e053ba1f48cc61
ChukwukaJ <117227123+ChukwukaJ@users.noreply.github.com>
2023-10-01 20:31:29 +0000

    lost

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,17 +1,9 @@
 #!/usr/bin/node
-const user = process.argv.length;
-const a = [];
-switch (user) {
-  case 2:
-  case 3:
-    console.log(0);
-    break;
-
-  default:
-    for (let i = 2; i < user; i++) {
-      a.push(process.argv[i]);
-    }
-    a.sort((a, b) => b - a);
-    console.log(a[1]);
-    break;
+if (process.argv.length <= 3) {
+  console.log(0);
+} else {
+  const args = process.argv.map(Number)
+    .slice(2, process.argv.length)
+    .sort((a, b) => a - b);
+  console.log(args[args.length - 2]);
 }
