c6b54c9d9ab66fa80504e72ff0ce6910ed193f85
ChukwukaJ <c.nwachukwu1@alustudent.com>
2023-12-16 19:56:38 +0000

    last

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -1,10 +1,14 @@
 #!/usr/bin/node
 const { argv } = require('process');
-const xTimes = parseInt(argv[2]);
-if (isNaN(xTimes)) {
-  console.log('Missing number of occurrences');
+const size = parseInt(argv[2]);
+if (isNaN(size)) {
+  console.log('Missing size');
 } else {
-  for (let i = 0; i < xTimes; i++) {
-    console.log('C is fun');
+  for (let i = 0; i < size; i++) {
+    let xString = '';
+    for (let j = 0; j < size; j++) {
+      xString = xString + 'X';
+    }
+    console.log(xString);
   }
 }
